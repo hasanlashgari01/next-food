0c1fba95f57f9bcabb24352f3dfb82af8b218342
Hasan Lashgari <hasanlashgari01@gmail.com>
2024-04-06 15:18:02 +0330

    fix: if user not login redirect to login page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
+import { IUser } from "./common/interface/user";
 
-const middlewareAuth = async (req: NextRequest) => {
+const middlewareAuth = async (req: NextRequest): Promise<IUser | null> => {
   let strCookie = "";
   req.cookies.getAll().forEach(item => {
     strCookie += `${item?.name}=${item?.value}; `;
@@ -14,22 +15,28 @@ const middlewareAuth = async (req: NextRequest) => {
     },
   }).then(res => res.json());
 
-  if (user?.status == 401) return { user: null };
+  if (user?.status == 401) return null;
 
-  return { user, role: user?.role };
+  return user;
 };
 
 export const middleware = async (req: NextRequest) => {
   const pathname = req.nextUrl.pathname;
+  const user = await middlewareAuth(req);
+  const redirectToLogin = () => NextResponse.redirect(new URL("/auth/login", req.url));
 
-  const { user } = await middlewareAuth(req);
-
+  //#region Redirects
   if (pathname.startsWith("/user") || pathname.startsWith("/cart")) {
-    if (!user) return NextResponse.redirect(new URL("/auth/login", req.url));
-  } else if (pathname.startsWith("/p-restaurant")) {
-    if (user) return NextResponse.redirect(new URL("/user", req.url));
-  } else if (pathname.startsWith("/admin")) {
-    if (!user) return NextResponse.redirect(new URL("/", req.url));
+    if (!user) return redirectToLogin();
+  }
+
+  if (pathname.startsWith("/p-restaurant")) {
+    if (!user) return redirectToLogin();
+    if (user?.role == "USER") return NextResponse.redirect(new URL("/user", req.url));
+  }
+
+  if (pathname.startsWith("/admin")) {
+    if (!user) return redirectToLogin();
     if (user && user.role !== "ADMIN") return NextResponse.redirect(new URL("/", req.url));
   }
 };
